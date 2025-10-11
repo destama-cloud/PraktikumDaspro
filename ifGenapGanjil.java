0e257f834dbc68261d4c753016a601358e323032
destama <insinfern@gmail.com>
2025-10-11 13:44:15 +0700

    Tugas 2

---
+++ ifGenapGanjil.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class ifGenapGanjil {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
+
+        if (angka % 2 == 0) {
+            System.out.println("Genap");
+        } else {
+            System.out.println("Ganjil");
+        }
+
+        input.close();
+    }
+}
